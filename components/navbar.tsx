b1edc49d3e03a7cc10f94f669e36505211a321f1
patel-mahek <mahekpatel2612@gmail.com>
2025-04-13 16:17:29 +0530

    name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold">
-              MyWebsite
+            Rediview
             </Link>
           </div>
           <div className="hidden md:block">
